51be47366537d045bd516e741d1a8cc27bfe90aa
Nelson Foltz <nelsfoltz@gmail.com>
2020-09-17 16:37:50 -0400

    add: refactor/extract routes to separate component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import DogList from './DogList';
-import DogDetails from './DogDetails';
 import Navbar from './Navbar';
-import { Switch, Route } from 'react-router-dom';
+import Routes from './Routes';
 import whiskey from './images/whiskey.jpg';
 import tubby from './images/tubby.jpg';
 import hazel from './images/hazel.jpg';
@@ -45,24 +43,10 @@ export class App extends Component {
     ]
   };
   render() {
-    const getDog = props => {
-      let name = props.match.params.name;
-      let currentDog = this.props.dogs.find(
-        dog => dog.name.toLowerCase() === name.toLowerCase()
-      );
-      return <DogDetails {...props} dog={currentDog} />;
-    };
     return (
       <div>
         <Navbar dogs={this.props.dogs} />
-        <Switch>
-          <Route
-            exact
-            path="/dogs"
-            render={() => <DogList dogs={this.props.dogs} />}
-          />
-          <Route exact path="/dogs/:name" render={getDog} />
-        </Switch>
+        <Routes dogs={this.props.dogs} />
       </div>
     );
   }
